9629944ddc26863a1bdadf6ed5d400be63de925b
sudeepbhagat11 <130081182+sudeepbhagat11@users.noreply.github.com>
2023-08-05 10:44:15 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      {notes.map((noteItem) => {
+        return <Note title={noteItem.title} content={noteItem.content} />;
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Note.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note(props) {
+  return (
+    <div className="note">
+      <p>{props.key}</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+    </div>
+  );
+}
+
+export default Note;
